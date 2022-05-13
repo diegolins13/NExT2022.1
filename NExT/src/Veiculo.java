0d44df07a64549c5c1587b3f55d42a3fc65a63de
Diego Lins <diegotec.lins@gmail.com>
2022-05-12 23:02:12 -0300

    Exercicio aula 8 Next

---
+++ NExT/src/Veiculo.java
@@ -0,0 +1,26 @@
+
+public class Veiculo {
+
+	public String marca;
+	public String modelo;
+	
+	public Veiculo(String marca, String modelo) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	
+}
